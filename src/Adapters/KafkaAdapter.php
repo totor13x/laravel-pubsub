ab323a43f9a4fa39ccbedf6e9c6d219390ada857
Fonseca <gdfonseca@leroymerlin.com.br>
2021-02-24 16:10:23 -0300

    add kafka adapter into laravel-pubsub

---
+++ src/Adapters/KafkaAdapter.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace LeroyMerlin\LaravelPubSub\Adapters;
+
+use Superbalist\PubSub\PubSubAdapterInterface;
+use Superbalist\PubSub\Utils;
+
+class KafkaAdapter implements PubSubAdapterInterface
+{
+    /**
+     * @var \RdKafka\Producer
+     */
+    protected $producer;
+
+    /**
+     * @var \RdKafka\KafkaConsumer
+     */
+    protected $consumer;
+
+    /**
+     * @param \RdKafka\Producer $producer
+     * @param \RdKafka\KafkaConsumer $consumer
+     */
+    public function __construct(\RdKafka\Producer $producer, \RdKafka\KafkaConsumer $consumer)
+    {
+        $this->producer = $producer;
+        $this->consumer = $consumer;
+    }
+
+    /**
+     * Return the Kafka producer.
+     *
+     * @return \RdKafka\Producer
+     */
+    public function getProducer()
+    {
+        return $this->producer;
+    }
+
+    /**
+     * Return the Kafka consumer.
+     *
+     * @return \RdKafka\KafkaConsumer
+     */
+    public function getConsumer()
+    {
+        return $this->consumer;
+    }
+
+    /**
+     * Subscribe a handler to a channel.
+     *
+     * @param string $channel
+     * @param callable $handler
+     *
+     * @throws \Exception
+     */
+    public function subscribe($channel, callable $handler)
+    {
+        $this->consumer->subscribe([$channel]);
+
+        $isSubscriptionLoopActive = true;
+
+        while ($isSubscriptionLoopActive) {
+            $message = $this->consumer->consume(120 * 1000);
+
+            if ($message === null) {
+                continue;
+            }
+
+            switch ($message->err) {
+                case RD_KAFKA_RESP_ERR_NO_ERROR:
+                    $payload = Utils::unserializeMessagePayload($message->payload);
+
+                    if ($payload === 'unsubscribe') {
+                        $isSubscriptionLoopActive = false;
+                    } else {
+                        call_user_func($handler, $payload);
+                    }
+
+                    $this->consumer->commitAsync($message);
+
+                    break;
+                case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                    break;
+                default:
+                    throw new \Exception($message->errstr(), $message->err);
+            }
+        }
+    }
+
+    /**
+     * Publish a message to a channel.
+     *
+     * @param string $channel
+     * @param mixed $message
+     */
+    public function publish($channel, $message)
+    {
+        $topic = $this->producer->newTopic($channel);
+        $topic->produce(RD_KAFKA_PARTITION_UA, 0, Utils::serializeMessage($message));
+    }
+
+    /**
+     * Publish multiple messages to a channel.
+     *
+     * @param string $channel
+     * @param array $messages
+     */
+    public function publishBatch($channel, array $messages)
+    {
+        foreach ($messages as $message) {
+            $this->publish($channel, $message);
+        }
+    }
+}
---
+++ tests/Adapters/KafkaAdapterTest.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace Tests\Adapters;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use LeroyMerlin\LaravelPubSub\Adapters\KafkaAdapter;
+use Tests\Adapters\Mocks\MockKafkaErrorMessage;
+
+if (!extension_loaded('rdkafka')) {
+    define('RD_KAFKA_PARTITION_UA', 0);
+
+    define('RD_KAFKA_RESP_ERR_NO_ERROR', 0);
+    define('RD_KAFKA_RESP_ERR__PARTITION_EOF', 1);
+    define('RD_KAFKA_RESP_ERR__TIMED_OUT', 2);
+}
+
+class KafkaAdapterTest extends TestCase
+{
+    public function testGetProducer()
+    {
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+        $adapter = new KafkaAdapter($producer, $consumer);
+        $this->assertSame($producer, $adapter->getProducer());
+    }
+
+    public function testGetConsumer()
+    {
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+        $adapter = new KafkaAdapter($producer, $consumer);
+        $this->assertSame($consumer, $adapter->getConsumer());
+    }
+
+    public function testSubscribeWithNullMessage()
+    {
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+
+        $consumer->shouldReceive('subscribe')
+            ->with(['channel_name'])
+            ->once();
+
+        // we need this to kill the infinite loop so the test can finish
+        $unsubscribeMessage = new \stdClass();
+        $unsubscribeMessage->err = RD_KAFKA_RESP_ERR_NO_ERROR;
+        $unsubscribeMessage->payload = 'unsubscribe';
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($unsubscribeMessage);
+
+        $consumer->shouldReceive('commitAsync')
+            ->with($unsubscribeMessage)
+            ->once();
+
+        $adapter = new KafkaAdapter($producer, $consumer);
+
+        $handler1 = Mockery::mock(\stdClass::class);
+        $handler1->shouldNotReceive('handle');
+
+        $adapter->subscribe('channel_name', [$handler1, 'handle']);
+    }
+
+    public function testSubscribeWithPartitionEofErrorCode()
+    {
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+
+        $consumer->shouldReceive('subscribe')
+            ->with(['channel_name'])
+            ->once();
+
+        $message = new \stdClass();
+        $message->err = RD_KAFKA_RESP_ERR__PARTITION_EOF;
+        $message->payload = null;
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($message);
+
+        $consumer->shouldNotReceive('commitAsnyc')
+            ->with($message);
+
+        // we need this to kill the infinite loop so the test can finish
+        $unsubscribeMessage = new \stdClass();
+        $unsubscribeMessage->err = RD_KAFKA_RESP_ERR_NO_ERROR;
+        $unsubscribeMessage->payload = 'unsubscribe';
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($unsubscribeMessage);
+
+        $consumer->shouldReceive('commitAsync')
+            ->with($unsubscribeMessage)
+            ->once();
+
+        $adapter = new KafkaAdapter($producer, $consumer);
+
+        $handler1 = Mockery::mock(\stdClass::class);
+        $handler1->shouldNotReceive('handle');
+
+        $adapter->subscribe('channel_name', [$handler1, 'handle']);
+    }
+
+    public function testSubscribeWithTimedOutErrorCode()
+    {
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+
+        $consumer->shouldReceive('subscribe')
+            ->with(['channel_name'])
+            ->once();
+
+        $message = new \stdClass();
+        $message->err = RD_KAFKA_RESP_ERR__TIMED_OUT;
+        $message->payload = null;
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($message);
+
+        $consumer->shouldNotReceive('commitAsnyc')
+            ->with($message);
+
+        // we need this to kill the infinite loop so the test can finish
+        $unsubscribeMessage = new \stdClass();
+        $unsubscribeMessage->err = RD_KAFKA_RESP_ERR_NO_ERROR;
+        $unsubscribeMessage->payload = 'unsubscribe';
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($unsubscribeMessage);
+
+        $consumer->shouldReceive('commitAsync')
+            ->with($unsubscribeMessage)
+            ->once();
+
+        $adapter = new KafkaAdapter($producer, $consumer);
+
+        $handler1 = Mockery::mock(\stdClass::class);
+        $handler1->shouldNotReceive('handle');
+
+        $adapter->subscribe('channel_name', [$handler1, 'handle']);
+    }
+
+    public function testSubscribeWithMessagePayload()
+    {
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+
+        $consumer->shouldReceive('subscribe')
+            ->with(['channel_name'])
+            ->once();
+
+        $message = new \stdClass();
+        $message->err = RD_KAFKA_RESP_ERR_NO_ERROR;
+        $message->payload = '{"hello":"world"}';
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($message);
+
+        $consumer->shouldReceive('commitAsync')
+            ->with($message)
+            ->once();
+
+        // we need this to kill the infinite loop so the test can finish
+        $unsubscribeMessage = new \stdClass();
+        $unsubscribeMessage->err = RD_KAFKA_RESP_ERR_NO_ERROR;
+        $unsubscribeMessage->payload = 'unsubscribe';
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($unsubscribeMessage);
+
+        $consumer->shouldReceive('commitAsync')
+            ->with($unsubscribeMessage)
+            ->once();
+
+        $adapter = new KafkaAdapter($producer, $consumer);
+
+        $handler1 = Mockery::mock(\stdClass::class);
+        $handler1->shouldReceive('handle')
+            ->with(['hello' => 'world'])
+            ->once();
+
+        $adapter->subscribe('channel_name', [$handler1, 'handle']);
+    }
+
+    public function testSubscribeWithErrorThrowsException()
+    {
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+
+        $consumer->shouldReceive('subscribe')
+            ->with(['channel_name'])
+            ->once();
+
+        $message = new MockKafkaErrorMessage();
+
+        $consumer->shouldReceive('consume')
+            ->with(120000)
+            ->once()
+            ->andReturn($message);
+
+        $consumer->shouldNotReceive('commitAsnyc')
+            ->with($message);
+
+        $adapter = new KafkaAdapter($producer, $consumer);
+
+        $handler1 = Mockery::mock(\stdClass::class);
+        $handler1->shouldNotReceive('handle');
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionCode(1234);
+        $this->expectExceptionMessage('This is an error message.');
+
+        $adapter->subscribe('channel_name', [$handler1, 'handle']);
+    }
+
+    public function testPublish()
+    {
+        $topic = Mockery::mock(\RdKafka\Topic::class);
+        $topic->shouldReceive('produce')
+            ->withArgs([
+                RD_KAFKA_PARTITION_UA,
+                0,
+                '{"hello":"world"}',
+            ])
+            ->once();
+
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+        $producer->shouldReceive('newTopic')
+            ->with('channel_name')
+            ->once()
+            ->andReturn($topic);
+
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+
+        $adapter = new KafkaAdapter($producer, $consumer);
+
+        $adapter->publish('channel_name', ['hello' => 'world']);
+    }
+
+    public function testPublishBatch()
+    {
+        $topic = Mockery::mock(\RdKafka\Topic::class);
+        $topic->shouldReceive('produce')
+            ->withArgs([
+                RD_KAFKA_PARTITION_UA,
+                0,
+                '{"hello":"world"}',
+            ])
+            ->once();
+        $topic->shouldReceive('produce')
+            ->withArgs([
+                RD_KAFKA_PARTITION_UA,
+                0,
+                '"lorem ipsum"',
+            ])
+            ->once();
+
+        $producer = Mockery::mock(\RdKafka\Producer::class);
+        $producer->shouldReceive('newTopic')
+            ->with('channel_name')
+            ->twice()
+            ->andReturn($topic);
+
+        $consumer = Mockery::mock(\RdKafka\KafkaConsumer::class);
+
+        $adapter = new KafkaAdapter($producer, $consumer);
+
+        $messages = [
+            ['hello' => 'world'],
+            'lorem ipsum',
+        ];
+        $adapter->publishBatch('channel_name', $messages);
+    }
+}
